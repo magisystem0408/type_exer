ccfef654e1c64b180ab04e93fd2149b2bbc415dc
magisystem0408 <masato.matsudo@gmail.com>
2021-09-29 02:17:28 +0900

    commigt

--- src/advanced.ts
+++ src/advanced.ts
@@ -23,4 +23,63 @@ type NumberBoolean = number | boolean;
 type StringNumber = string | number
 
 // ベン図
-type Mix =NumberBoolean & StringNumber;
+type Mix = NumberBoolean & StringNumber;
+
+
+function toUpperCase(x: string | number) {
+    // 型チェック
+    if (typeof x === "string") {
+        return x.toUpperCase()
+    }
+    return ""
+
+}
+
+type NomadWorker = Engineer | Blogger
+
+function describeNomadWorkerProfile(nomadWorker: NomadWorker) {
+
+    // nameだけアクセスができる
+    //両方持っているものしかアクセスできない
+    console.log(nomadWorker.name)
+
+    // 存在するか
+    if ("role" in nomadWorker) {
+        console.log(nomadWorker.role)
+    }
+
+    if ("follower" in nomadWorker) {
+        console.log(nomadWorker.follower)
+    }
+
+}
+
+class Dog {
+    speak() {
+        console.log("bow-wow")
+    }
+}
+
+class Bird {
+    speak() {
+        console.log("tweet")
+    }
+
+    fly() {
+        console.log("flutter")
+    }
+}
+
+type Pet = Dog | Bird;
+
+function havePet(pet:Pet){
+    pet.speak()
+
+    // 関数の指定
+    // Birdクラスから生成されたpetであるならば出力
+    if (pet instanceof Bird){
+        pet.fly()
+    }
+}
+
+
--- src/class.ts
+++ src/class.ts
@@ -1,31 +1,74 @@
-class Person {
+abstract class Person {
+
+    static species = "homo sapiens"
 
     // 読み書きのみ
     readonly id: number = 32;
 
     //protectedとすると継承先で使用される
 
-    constructor(public readonly name: string, protected  age: number) {
+    constructor(public readonly name: string, protected age: number) {
         // readonlyでもここでは書き換えられる
         this.id = 32;
     }
 
     incrementAge() {
+        // インスタンス化されていないのでthisが使えない。
+        Person.species = "test"
         this.age += 1;
     }
+
+    // 継承先で入れらないといけない
+    abstract explainJob():void
 
 }
 
 class Teacher extends Person {
-    constructor(name: string, age: number) {
+    explainJob() {
+        console.log("継承先で使用しないといけない")
+    }
+
+    // ゲッターは何か返さないと行けない
+    get subject(): string {
+        // 空文字だった場合
+        if (!this._subject) {
+            throw new Error("There is no subject")
+        }
+        return this._subject
+    }
+
+    // 絶対に一つの値を入れる必要がある
+    // getの返り値を推測してくれる
+
+    set subject(value) {
+        if (!value) {
+            throw  new Error("There is no subject")
+        }
+        this._subject = value;
+    }
+
+    constructor(name: string, age: number, private _subject: string) {
+        // 継承前のコンストラクタを出している
         super(name, age);
     }
 
-    greetting(){
+    greetting() {
         console.log(`${this.incrementAge()}`)
     }
 }
 
 const quill = new Person("quill", 38)
-
 quill.incrementAge()
+
+
+const teacher = new Teacher("timi", 230, "math")
+console.log(teacher.subject)
+
+// setterで格納する
+teacher.subject = "test"
+
+teacher.greetting()
+
+
+// pythonの@staticmethodと同じ
+console.log(Teacher.species)
